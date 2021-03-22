7868ce17dfbfe99c3a46f110ce80c8f1e741070b
Rahul Jangid <76434928+rahuljangid12051998@users.noreply.github.com>
2021-03-22 15:53:21 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,10 @@
+import math
+radius = int(input("enter the radius: "))
+pie = float(3.14170)
+if radius<31:
+    area = (pie*radius*radius)
+    print(area)
+else:
+    print("Radius out of range")
+
+print(math.pi)
